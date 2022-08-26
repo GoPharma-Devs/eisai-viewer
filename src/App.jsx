03edc90e1447c93ce9918c9bbd1e30e2545f1a02
Carlos Boyzo <carlosboyzo@MacBook-Pro-de-Carlos-2.local>
2022-08-26 18:54:23 -0500

    ajuste

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
 
-<div ><iframe src="https://player.vimeo.com/video/743591861?h=d87c968eef&badge=0&autopause=0&player_id=0&app_id=58479" frameBorder={0} allow="autoplay; fullscreen; picture-in-picture" allowFullScreen style={{position: 'absolute', top: 0, left: 0, width: '100%', height: '100%'}} title="Evom_Highlights.mp4" /></div>
+<div className='video' ><iframe src="https://player.vimeo.com/video/743591861?h=d87c968eef&badge=0&autopause=0&player_id=0&app_id=58479" frameBorder={0} allow="autoplay; fullscreen; picture-in-picture" allowFullScreen style={{position: 'absolute', top: '12%', left: '12%', width: '77%', height: '79%'}} title="Ahora disponible video memoria EVOM 2022 " /></div>
 
 
     </div>
